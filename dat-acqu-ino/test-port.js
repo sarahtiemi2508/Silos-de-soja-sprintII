e149be0adcdf47fe4f7cac2f12acfbce9eeb554c
MarcellyPereira007 <marcelly.silva@sptech.school>
2026-04-25 22:05:47 -0300

    CAMINHOS - Alterando caminho do index do datacquino

--- dat-acqu-ino/test-port.js
+++
@@ -1,6 +0,0 @@
-const { SerialPort } = require('serialport')
-SerialPort.list().then(ports => {
-  ports.forEach(port => console.log(port.path, port.pnpId, port.manufacturer))
-})
-
-//Testar a porta e qual o id
