ee9c61d74adb37fff7eb9a9460634077e9c11f2b
MarcellyPereira007 <marcelly.silva@sptech.school>
2026-05-18 23:24:51 -0300

    Correções de front para cad e login

--- site-institucional/src/routes/home.js
+++ site-institucional/src/routes/home.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function (req, res) {
-    res.render("home");
+    res.send("A API do Projeto Ceres está online e funcionando perfeitamente!");
 });
 
 module.exports = router;
