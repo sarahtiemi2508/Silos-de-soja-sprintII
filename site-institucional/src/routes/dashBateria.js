784401fab28415f591b6d9cbe6364d4c5f8c30fb
sarahtiemi2508 <sarah.sato@sptech.school>
2026-06-08 00:29:49 -0300

    Atualizando dashBateria.js

--- site-institucional/src/routes/dashBateria.js
+++ site-institucional/src/routes/dashBateria.js
@@ -3,25 +3,33 @@ var router = express.Router();
 
 var dashBateriaController = require("../controllers/dashBateriaController");
 
-router.get("/selectVolumeTotal/:id", function (req, res) {
+router.get("/selectInfoBateria", function (req, res) {
+    dashBateriaController.selectInfoBateria(req, res);
+});
+
+router.get("/selectInfoFazenda", function (req, res) {
+    dashBateriaController.selectInfoFazenda(req, res);
+});
+
+router.get("/selectVolumeTotal", function (req, res) {
     dashBateriaController.selectVolumeTotal(req, res);
 });
 
-router.get("/selectOcupacaoTotal/:id", function (req, res) {
-    dashBateriaController.selectOcupacaoTotal(req, res);
-});
 
-router.get("/selectMaiorNivel/:id", function (req, res) {
+router.get("/selectMaiorNivel", function (req, res) {
     dashBateriaController.selectMaiorNivel(req, res);
 });
 
-router.get("/selectMenorNivel/:id", function (req, res) {
+router.get("/selectMenorNivel", function (req, res) {
     dashBateriaController.selectMenorNivel(req, res);
 });
 
-router.get("/selectVolumeMensalBateria/:id", function (req, res) {
+router.get("/selectVolumeMensalBateria", function (req, res) {
     dashBateriaController.selectVolumeMensalBateria(req, res);
 });
 
+router.get("/selectInfoSiloIndividual", function (req, res) {
+    dashBateriaController.selectInfoSiloIndividual(req, res);
+});
 
 module.exports = router;
