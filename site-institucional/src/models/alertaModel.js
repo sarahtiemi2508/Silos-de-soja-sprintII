9136307e043f510ee758e1e0673e183e2430925b
AnaReis-SPTech <ana.reis@sptech.school>
2026-06-09 08:30:04 -0300

    Corrigindo ultimos detalhes

--- site-institucional/src/models/alertaModel.js
+++ site-institucional/src/models/alertaModel.js
@@ -4,6 +4,7 @@ var database = require("../database/config");
 function listarAlertas(id_usuario) {
     var instrucao = `
         SELECT
+            iss.id_alerta,
             iss.id_bateria,
             iss.id_fazenda,
             iss.id_silo_individual,
@@ -27,6 +28,7 @@ function listarAlertas(id_usuario) {
 function listarAlertasEmpresa(id_empresa) {
     var instrucao = `
         SELECT
+            iss.id_alerta,
             iss.situacao,
             iss.num_silo,
             iss.fazenda,
